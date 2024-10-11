2c64e172af7372e06f5c6e9659dbf08e107175e4
sebastian-VB <sebas99peru@gmail.com>
2024-10-11 10:45:21 -0500

    Add routerlink property in menuItens

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -10,6 +10,8 @@ export class MenuComponent implements OnInit {
 
   menuItems: MenuItem[] = [];
 
+  constructor(){}
+
   ngOnInit(): void {
     this.menuItems = [
       {
@@ -18,15 +20,18 @@ export class MenuComponent implements OnInit {
         items: [
           {
             label: 'Textos y fechas',
-            icon: 'pi pi-align-left'
+            icon: 'pi pi-align-left',
+            routerLink: '/'
           },
           {
             label: 'Números',
-            icon: 'pi pi-dollar'
+            icon: 'pi pi-dollar',
+            routerLink: 'numbers'
           },
           {
             label: 'No comunes',
-            icon: 'pi pi-globe'
+            icon: 'pi pi-globe',
+            routerLink: 'uncommon'
           },
         ]
       },
