5a5b9b5073036856d752327978a63a2db8249319
sebastian-VB <sebas99peru@gmail.com>
2024-10-11 17:28:45 -0500

    Add imports of languages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+//Configuracion del locale de la app
+import localEsPE from '@angular/common/locales/es-PE';
+
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localEsPE);
 
 @NgModule({
   declarations: [
@@ -16,7 +23,9 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-PE'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
